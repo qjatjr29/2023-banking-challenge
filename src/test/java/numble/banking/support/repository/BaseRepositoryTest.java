d5ec17b0cd8b0c10a6b87034946101768f9c8920
beomsic <qjatjr29@khu.ac.kr>
2023-02-11 16:40:51 +0900

    :test_tube: test: repository 테스트를 위한 base class 생성

---
+++ src/test/java/numble/banking/support/repository/BaseRepositoryTest.java
@@ -0,0 +1,13 @@
+package numble.banking.support.repository;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class BaseRepositoryTest {
+
+}

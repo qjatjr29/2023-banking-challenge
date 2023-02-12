f07f7803bac9b364099e4196beccebc40f5acf4e
beomsic <qjatjr29@khu.ac.kr>
2023-02-12 19:58:56 +0900

    :sparkles: feat: 토큰 갱신 로직, api 추가

--- src/main/java/numble/banking/core/user/presentation/UserAuthController.java
+++ src/main/java/numble/banking/core/user/presentation/UserAuthController.java
@@ -2,6 +2,7 @@ package numble.banking.core.user.presentation;
 
 import javax.validation.Valid;
 import numble.banking.core.user.command.application.LoginRequest;
+import numble.banking.core.user.command.application.TokenReissueRequest;
 import numble.banking.core.user.command.application.TokenResponse;
 import numble.banking.core.user.command.application.UserAuthService;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,12 @@ public class UserAuthController {
   @PostMapping("/login")
   public ResponseEntity<TokenResponse> login(@RequestBody @Valid LoginRequest request) {
     TokenResponse tokenInfo = userAuthService.login(request);
+    return ResponseEntity.ok().body(tokenInfo);
+  }
+
+  @PostMapping("/reissue")
+  public ResponseEntity<TokenResponse> reissue(@RequestBody TokenReissueRequest request) {
+    TokenResponse tokenInfo = userAuthService.reissue(request);
     return ResponseEntity.ok().body(tokenInfo);
   }
 }

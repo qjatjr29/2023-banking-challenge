5a4551816c4c37663015b9a4792930dfdd3e4223
beomsic <qjatjr29@khu.ac.kr>
2023-02-12 15:36:37 +0900

    :sparkles: feat: 로그인 api 추가

---
+++ src/main/java/numble/banking/core/user/presentation/UserAuthController.java
@@ -0,0 +1,28 @@
+package numble.banking.core.user.presentation;
+
+import javax.validation.Valid;
+import numble.banking.core.user.command.application.LoginRequest;
+import numble.banking.core.user.command.application.TokenResponse;
+import numble.banking.core.user.command.application.UserAuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/authentication")
+public class UserAuthController {
+
+  private final UserAuthService userAuthService;
+
+  public UserAuthController(UserAuthService userAuthService) {
+    this.userAuthService = userAuthService;
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<TokenResponse> login(@RequestBody @Valid LoginRequest request) {
+    TokenResponse tokenInfo = userAuthService.login(request);
+    return ResponseEntity.ok().body(tokenInfo);
+  }
+}

11ac69b62bdf7dbf43de9c3cc12867bd96edbf7e
beomsic <qjatjr29@khu.ac.kr>
2023-02-14 16:31:20 +0900

    :sparkles: feat: 계좌 관련 dto

---
+++ src/main/java/numble/banking/core/account/command/application/OpenAccountRequest.java
@@ -0,0 +1,25 @@
+package numble.banking.core.account.command.application;
+
+import javax.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import numble.banking.core.account.command.domain.AccountType;
+import numble.banking.core.account.command.domain.Bank;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class OpenAccountRequest {
+
+  @NotBlank
+  private AccountType accountType;
+
+  @NotBlank
+  private String accountName;
+
+  @NotBlank
+  private Bank bankName;
+
+}

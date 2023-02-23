9e59c6a06f5037ddef4a6983856d3e3f9d94c2cf
beomsic <qjatjr29@khu.ac.kr>
2023-02-23 18:13:36 +0900

    :sparkles: feat: 서버 시간 설정

--- src/main/java/numble/banking/BankingApplication.java
+++ src/main/java/numble/banking/BankingApplication.java
@@ -1,5 +1,7 @@
 package numble.banking;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +9,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class BankingApplication {
+
+	@PostConstruct
+	public void start() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingApplication.class, args);

20219c23e254c5240dc0ce6d942f8b8c46d057dd
beomsic <qjatjr29@khu.ac.kr>
2023-02-25 20:29:01 +0900

    :sparkles: feat: CORS관련 필터 추가

---
+++ src/main/java/numble/banking/core/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package numble.banking.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.addAllowedOrigin("*");
+    config.addAllowedMethod("*");
+    config.addAllowedHeader("*");
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+  }
+
+}

95248a2c02ec258f8bd62d15106b186594758b66
beomsic <qjatjr29@khu.ac.kr>
2023-02-25 20:30:38 +0900

    :sparkles: feat: 유저 상세 정보 조회 dto 수정

--- src/main/java/numble/banking/core/user/query/dto/UserQueryDetailResponse.java
+++ src/main/java/numble/banking/core/user/query/dto/UserQueryDetailResponse.java
@@ -1,5 +1,7 @@
 package numble.banking.core.user.query.dto;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,21 +10,35 @@ import numble.banking.core.user.command.domain.Address;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class UserQueryDetailResponse {
 
+  private Long id;
   private String loginId;
   private String name;
   private String email;
   private String phone;
   private Address address;
+  private LocalDate createdAt;
+
+  public UserQueryDetailResponse(Long id, String loginId, String name, String email, String phone,
+      Address address, LocalDateTime createdAt) {
+    this.id = id;
+    this.loginId = loginId;
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.address = address;
+    this.createdAt = LocalDate.from(createdAt);
+  }
 
   public UserQueryDetailResponse(final UserData userData) {
+    this.id = userData.getId();
     this.loginId = userData.getLoginId();
     this.name = userData.getName();
     this.email = userData.getEmail();
     this.phone = userData.getPhone();
     this.address = userData.getAddress();
+    this.createdAt = LocalDate.from(userData.getCreatedAt());
   }
 
   public static UserQueryDetailResponse of(final UserData userData) {

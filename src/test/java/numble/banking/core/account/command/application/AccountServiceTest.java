e7437a15466efaa0fd59ea8962e0cd728ac36cc3
beomsic <qjatjr29@khu.ac.kr>
2023-02-27 10:18:27 +0900

    :test_tube: test: 이체 테스트

---
+++ src/test/java/numble/banking/core/account/command/application/AccountServiceTest.java
@@ -0,0 +1,118 @@
+package numble.banking.core.account.command.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import numble.banking.core.account.command.domain.Account;
+import numble.banking.core.account.command.domain.AccountRepository;
+import numble.banking.core.account.command.domain.AccountType;
+import numble.banking.core.account.command.domain.Bank;
+import numble.banking.core.account.command.domain.Money;
+import numble.banking.core.user.command.application.UserService;
+import numble.banking.core.user.command.domain.Address;
+import numble.banking.core.user.command.domain.User;
+import numble.banking.core.user.command.domain.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@ExtendWith({SpringExtension.class})
+@DisplayName("계좌 서비스 테스트")
+class AccountServiceTest {
+
+  @Autowired
+  AccountService accountService;
+
+  @Autowired
+  TransferService transferService;
+
+  @Autowired
+  UserRepository userRepository;
+
+  @Autowired
+  AccountRepository accountRepository;
+
+  @Autowired
+  UserService userService;
+
+  User user;
+  User friend;
+  Account userAccount;
+  Account friendAccount;
+
+  @BeforeEach
+  void setup() {
+
+    Long userId = 1L;
+    Long friendId = 2L;
+
+    user = userRepository.save(User.builder()
+        .id(userId)
+        .name("beomsic")
+        .email("beomsic@gmail.com")
+        .phone("010-0000-0000")
+        .build());
+
+    friend = userRepository.save(User.builder()
+        .id(friendId)
+        .name("friend")
+        .email("friend@gmail.com")
+        .phone("010-1234-4456")
+        .address(Address.from("03333", "서울시", "서울시"))
+        .build());
+
+    userAccount = accountRepository.save(Account.builder()
+        .userId(user.getId())
+        .ownerName(user.getName())
+        .accountName("beomsic_account")
+        .accountNumber("1000-000-000001")
+        .balance(new Money(10000L))
+        .bank(Bank.국민은행)
+        .accountType(AccountType.DEPOSIT)
+        .build());
+
+    friendAccount = accountRepository.save(Account.builder()
+        .userId(friend.getId())
+        .ownerName(friend.getName())
+        .accountName("friend_account")
+        .accountNumber("1000-000-000002")
+        .balance(new Money(10000L))
+        .bank(Bank.우리은행)
+        .accountType(AccountType.DEPOSIT)
+        .build());
+  }
+
+  @Test
+  @Transactional
+  @DisplayName("이체 테스트")
+  void transferMoney(){
+
+    // given
+    TransferRequest transferRequest = new TransferRequest(
+        userAccount.getId(),
+        friendAccount.getId(),
+        new Money(1000L),
+        friendAccount.getAccountNumber(),
+        "transfer test");
+
+    // when
+    userService.follow(user.getId(), friend.getId());
+    TransferResponse transferResponse = accountService.transfer(user.getId(), transferRequest);
+
+    // then
+    assertEquals(user.getName(), transferResponse.getFrom());
+    assertEquals(friend.getName(), transferResponse.getTo());
+    assertEquals(9000L, userAccount.getBalance().getMoney());
+    assertEquals(11000L, friendAccount.getBalance().getMoney());
+    assertEquals(1, userAccount.getTransferHistories().size());
+  }
+}

ceacbe6ac2468725abd8e0b43ee432c14a879dd4
beomsic <qjatjr29@khu.ac.kr>
2023-02-28 13:32:31 +0900

    :sparkles: feat: 계좌이체성공이벤트 핸들러 빈으로 등록

--- src/main/java/numble/banking/core/account/command/application/TransferCompletedEventHandler.java
+++ src/main/java/numble/banking/core/account/command/application/TransferCompletedEventHandler.java
@@ -1,11 +1,14 @@
 package numble.banking.core.account.command.application;
 
+import lombok.extern.slf4j.Slf4j;
 import numble.banking.core.account.command.domain.TransferCompletedEvent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 
-
+@Slf4j
+@Component
 public class TransferCompletedEventHandler {
 
   @Value("${kafka.topic.transfer-completed}")
